87cd11e4aee22001fdbf8e1dc7f6ef4743db4373
xiaoxinfish <xiao_xin_fish@outlook.com>
2018-09-28 22:33:11 +0800

    clean up the folder

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from src.simpleLinearRegression import SimpleLinearRegression as SimpleLR
+import numpy as np
+
+if __name__ == '__main__':
+    xSet = np.array([1, 2, 2.5, 3, 5])
+    ySet = np.array([2, 3.3, 4, 4.9, 6])
+    sLR = SimpleLR(xSet, ySet, 3.2)
+    sLR.predict()
+    sLR.info()
+    sLR.show()
+

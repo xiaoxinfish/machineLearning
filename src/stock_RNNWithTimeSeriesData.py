33cce49ba8b4f13693cb303e6d08f87e4d7b0ea4
lgzhao <lei.g.zhao@nokia-sbell.com>
2018-10-18 16:33:38 +0800

    code compiled succeeded. But the matplotlab simulator show nothing

--- src/stock_RNNWithTimeSeriesData.py
+++ src/stock_RNNWithTimeSeriesData.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
 import tensorflow as tf
 import numpy as np
-print('hello')
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import minmax_scale
 
+print('hello world!')
 
 # # Reading data
-
-# In[ ]:
-
 
 timesteps = seq_length = 7
 data_dim = 5
 output_dim =1
-
-
-# In[ ]:
-
-
 # Open,High,Low,Close,Volume
-xy = np.loadtxt('data-02-stock_daily.csv', delimiter = ',')
+xy = np.loadtxt('sampleStockData.csv', delimiter = ',')
 xy = xy[::-1] #reverse order (chronically ordered)
-xy = MinMaxScaler(xy)
+
+xy = minmax_scale(xy)
+
 x = xy
 y = xy[:, [-1]] #close as label
 
 dataX = []
 dataY = []
-for i in range(0, len(y), - seq_length):
+for i in range(0, len(y) - seq_length):
     _x = x[i:i + seq_length]
     _y = y[i + seq_length]
     print(_x, "->", _y)
@@ -39,34 +32,29 @@ for i in range(0, len(y), - seq_length):
     dataY.append(_y)
 
 
-# # Training and test datasets
 
-# In[ ]:
+# # Training and test datasets
 
 
 # split to train and testing
 train_size = int(len(dataY) * 0.7)
-test_size = len(data_Y) - train_size
-trainX, testX = np.array(dataX[0:train_size]),
-                np.array(dataX[train_size:len(dataX)])
-trainX, testX = np.array(dataY[0:train_size]),
-                np.array(dataY[train_size:len(dataY)])
+test_size = len(dataY) - train_size
+trainX, testX = np.array(dataX[0:train_size]), np.array(dataX[train_size:len(dataX)])
+trainY, testY = np.array(dataY[0:train_size]), np.array(dataY[train_size:len(dataY)])
 #input placeholders
 X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
-Y = tf.placeholder(tf.float32, [None, 1 ])
+Y = tf.placeholder(tf.float32, [None, 1])
 
 
 # # LSTM and Loss
-
-# In[ ]:
 
 
 # input placeholders
 X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
 Y = tf.placeholder(tf.float32, [None,1])
 
-cell = tf.contrib.rnn.BasicLSTMCell(num_units = output_dim, state_is_tuple = True)
-outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype - tf.float32)
+cell = tf.nn.rnn_cell.LSTMCell(name = 'basic_lstm_cell', num_units = output_dim, state_is_tuple = True)
+outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype = tf.float32)
 Y_pred = outputs[:, -1] #we use the last cell's output
 
 #cost/lost
@@ -78,20 +66,18 @@ train = optimizer.minimize(loss)
 
 # # Training and Results
 
-# In[ ]:
-
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for i in range(1000):
-    _, l = sess.run([train, loss]),
-        feed_dict = {X: trainX, Y: trainY})
+    _, l = sess.run([train, loss], feed_dict = {X: trainX, Y: trainY})
     print(i, l)
 
-testPredict = sess.run(Y_pred, fee_dict = {X: testX})
+testPredict = sess.run(Y_pred, feed_dict = {X: testX})
 
 plt.plot(testY)
 plt.plot(testPredict)
 plt.show()
+
 

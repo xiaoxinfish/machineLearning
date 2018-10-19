52b7689fc6590309f7a3372f3ae30e5aae6d83f9
lgzhao <lei.g.zhao@nokia-sbell.com>
2018-10-19 13:33:09 +0800

    Ver 1.0 000016 predicted

--- src/stock_RNNWithTimeSeriesData.py
+++ src/stock_RNNWithTimeSeriesData.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import logging
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import os.path
+import time
+import stockPreProcess as spp
 from sklearn.preprocessing import minmax_scale
 
+logger = logging.getLogger()
+logger.setLevel(logging.NOTSET)
+rq = time.strftime('%Y%m%d', time.localtime(time.time()))
+logPath = os.path.dirname(os.getcwd()) + '/logs/'
+logName = logPath + rq + '.log'
+fh = logging.FileHandler(logName, mode = 'w')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 print('hello world!')
 
 # # Reading data
@@ -13,20 +24,22 @@ print('hello world!')
 timesteps = seq_length = 7
 data_dim = 5
 output_dim =1
-# Open,High,Low,Volume,Close
-import stockPreProcess as spp
-import time
+# get clear up data
+
+
 stockCode = '000016'
 originalFileName = 'stockData/000016_20181018.txt'
+# Open,High,Low,Volume,Close
 CSVFileName = 'stockData/' + stockCode + '_Daily_' + time.strftime('%Y-%m-%d', time.localtime(time.time()))  + '.csv'
 sd = spp.stock()
 sd.readOriginalData(stockCode, originalFileName)
 sd.saveToCSV(stockCode, CSVFileName)
-
 xy = np.loadtxt(CSVFileName, delimiter = ',')
+
 
 xy = xy[::-1] #reverse order (chronically ordered)
 print(xy)
+logger.info(xy)
 xy = minmax_scale(xy)
 
 x = xy
@@ -37,7 +50,8 @@ dataY = []
 for i in range(0, len(y) - seq_length):
     _x = x[i:i + seq_length]
     _y = y[i + seq_length]
-    print(_x, "->", _y)
+    print('{} -> {}'.format(_x, _y))
+    logger.debug('{} -> {}'.format(_x, _y))
     dataX.append(_x)
     dataY.append(_y)
 
@@ -50,6 +64,7 @@ for i in range(0, len(y) - seq_length):
 train_size = int(len(dataY) * 0.7)
 test_size = len(dataY) - train_size
 print("test date size:{}".format(test_size))
+logging.debug("test date size:{}".format(test_size))
 trainX, testX = np.array(dataX[0:train_size]), np.array(dataX[train_size:len(dataX)])
 trainY, testY = np.array(dataY[0:train_size]), np.array(dataY[train_size:len(dataY)])
 #input placeholders
@@ -84,11 +99,12 @@ sess.run(tf.global_variables_initializer())
 for i in range(1000):
     _, l = sess.run([train, loss], feed_dict = {X: trainX, Y: trainY})
     print(i, l)
+    logging.debug('{}, {}'.format(i, l))
 
 testPredict = sess.run(Y_pred, feed_dict = {X: testX})
 plt.plot(testY, 'r')
 plt.plot(testPredict, 'b')
-
 plt.show()
+logger.removeHandler(fh)
 
 

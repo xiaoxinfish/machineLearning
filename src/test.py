f20220b152c0dc37c6af180dc25b281fe6dd7888
xiaoxinfish <xiao_xin_fish@outlook.com>
2018-10-21 19:59:31 +0800

    代码调试成功。 但是： 1>原代码中，交易数据，在x轴，也就是时间序列上，被倒置了。导致，绘图时，图形是x轴反向的。目前代码，去除了倒置。不知道为什么要倒置。 2>输出实际收盘价与预测收盘价时，预测值，在图上，延迟一天。目前代码，强制对齐。不知道这个延迟，到底时代表什么

--- src/test.py
+++ src/test.py
@@ -1,5 +1,16 @@
 import stockPreProcess as spp
+import numpy as np
 
-sd = spp.stock()
-sd.readOriginalData('000016', 'stockData/000016_20181018.txt')
-sd.saveToCSV('000016')
+def convStockDataToCSV():
+    sd = spp.stock()
+    sd.readOriginalData('000016', 'stockData/000016_20181018.txt')
+    sd.saveToCSV('000016')
+
+def arrayAppend():
+    y = [1.2,3.1,4.5,5.1,6.1]
+    print(y)
+    y = y[2:]
+    print(y)
+
+if __name__ == '__main__':
+    arrayAppend()
